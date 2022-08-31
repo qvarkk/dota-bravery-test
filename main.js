2517dc1fe17e6f126219d34c5e3cc3ac5e37afe2
qvarkk <xumdes@gmail.com>
2022-08-31 14:47:58 +0500

    Added heroes and skills to randomizer

--- main.js
+++ main.js
@@ -10,6 +10,8 @@ const boots = ['Power Treads', 'Phase Boots', 'Boots Of Travel', 'Arcane Boots',
 const items = ['Ghost Scepter', 'Blink Dagger', 'Mask Of Madness', 'Hand Of Midas', 'Helm Of The Dominator', 'Moon Shard', 'Helm Of The Overlord', 'Drum Of Endurance', 'Mekansm', 'Holy Locket', "Vladmir's Offering", 'Spirit Vessel', 'Pipe Of Insight', 'Guardian Greaves', 'Boots Of Bearing', 'Wraith Pact', 'Veil Of Discord', 'Glimmer Cape', 'Force Staff', 'Aether Lens', 'Witch Blade', "Eul's Scepter Of Divinity", 'Rod Of Atos', 'Dagon Lvl 1', 'Dagon Lvl 2', 'Dagon Lvl 3', 'Dagon Lvl 4', 'Orchid Malevolence', 'Solar Crest', "Aghanim's Scepter", 'Refresher Orb', 'Octarine Core', 'Scythe Of Vyse', 'Gleipnir', 'Wind Waker', 'Hood Of Defiance', 'Vanguard', 'Blade Mail', 'Aeon Disk', 'Eternal Shroud', 'Crimson Guard', 'Lotus Orb', 'Black King Bar', 'Hurricane Pike', 'Manta Style', "Linken's Sphere", "Shiva's Guard", 'Heart Of Tarrasque', 'Assault Cuirass', 'Bloodstone', 'Crystalys', 'Meteor Hammer', 'Armlet Of Mordiggian', 'Skull Basher', 'Shadow Blade', 'Desolator', 'Battle Fury', 
 'Ethereal Blade', 'Nullifier', 'Monkey King Bar', 'Butterfly', 'Radiance', 'Daedalus', 'Silver Edge', 'Bloodthorn', 'Abyssal Blade', "Revenant's Brooch", 'Dragon Lance', 'Sange', 'Yasha', 'Kaya', 'Echo Sabre', 'Maelstrom', 'Diffusal Blade', 'Mage Slayer', "Heaven's Halberd", 'Kaya And Sange', 'Sange And Yasha', 'Yasha And Kaya', 'Satanic', 'Eye Of Skadi', 'Mjollnir', 'Overwhelming Blink', 'Swift Bling', 'Arcane Blink', 'Divine Rapier', 'Gem of True Sight'];
 
+const heroPara = document.querySelector('#heroPara');
+const skillPara = document.querySelector('#skillPara');
 const tempItemPara = document.querySelector('#tempItemPara');
 const bootsItemPara = document.querySelector('#bootsItemPara');
 const firstItemPara = document.querySelector('#firstItemPara');
@@ -36,6 +38,8 @@ function randomize() {
 }
 
 function changeToNewItems() {
+  heroPara.textContent = hero;
+  skillPara.textContent = maxFirst;
   tempItemPara.textContent = tempItem;
   bootsItemPara.textContent = bootsItem;
   firstItemPara.textContent = firstItem;
